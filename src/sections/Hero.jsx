3923da47d6566492d8a72f3b4b073a6db17188fa
bruno <bpires.siqueira@gmail.com>
2025-11-12 15:56:38 -0300

    fix: hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,5 @@
-import { Canvas, useFrame } from "@react-three/fiber";
+import { motion } from "motion/react";
+import { Canvas } from "@react-three/fiber";
 import HeroText from "../components/HeroText";
 import ParallaxBackground from "../components/ParallaxBackground";
 import { Computer } from "../components/Computer";
@@ -8,6 +9,7 @@ import * as THREE from "three";
 
 const Hero = () => {
   const isMobile = useMediaQuery({ maxWidth: 768 });
+
   return (
     <section
       className={`flex ${
@@ -24,21 +26,34 @@ const Hero = () => {
           isMobile ? "h-[300px] mt-1" : "h-[600px] mt-20"
         }`}
       >
-        <Canvas camera={{ position: [0, 1, 3] }}>
-          <ambientLight intensity={3} />
-          <directionalLight position={[2, 2, 2]} intensity={1} />
-          <Computer scale={isMobile ? 0.6 : 0.45} />
-          <OrbitControls
-            enableZoom={false}
-            enablePan={false}
-            mouseButtons={{
-              LEFT: THREE.MOUSE.ROTATE,
-              RIGHT: null,
-            }}
-          />
-        </Canvas>
+        <motion.div
+          initial={{ y: -200, opacity: 0 }} // começa fora da tela, no topo
+          animate={{ y: 0, opacity: 1 }} // desce pro lugar e aparece
+          transition={{
+            type: "spring", // efeito elástico
+            stiffness: 80,
+            damping: 15,
+            duration: 2,
+          }}
+          className="w-full h-full"
+        >
+          <Canvas camera={{ position: [0, 1, 3] }}>
+            <ambientLight intensity={3} />
+            <directionalLight position={[2, 2, 2]} intensity={1} />
+            <Computer scale={isMobile ? 0.6 : 0.45} />
+            <OrbitControls
+              enableZoom={false}
+              enablePan={false}
+              mouseButtons={{
+                LEFT: THREE.MOUSE.ROTATE,
+                RIGHT: null,
+              }}
+            />
+          </Canvas>
+        </motion.div>
       </figure>
     </section>
   );
 };
+
 export default Hero;
